93b62d4e72d20398bf1b0a1d2b4fcab2be5f1f50
Wanderson <lopez.wanderson@gmail.com>
2018-10-03 11:05:47 -0600

    Configuración de la conexión hacia Postgre

--- djangoFirstApp/settings.py
+++ djangoFirstApp/settings.py
@@ -75,8 +75,14 @@ WSGI_APPLICATION = 'djangoFirstApp.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        #'ENGINE': 'django.db.backends.sqlite3',
+        #'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'polls',
+        'USER':'postgres',
+        'PASSWORD': 'wanderlp',
+        'HOST': '127.0.0.1',
+        'PORT': '5432',
     }
 }
 
